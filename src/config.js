53079b9e50b476b8bf5ae0c71a769579064fefaa
Jimmy <antonka@vk.com>
2025-10-01 20:35:17 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ module.exports = {
   PROMPT_TEMPLATE_PATH: path.join(__dirname, 'prompt_templates/analyst_prompt.txt'),
   SEMANTIC_DICT_PATH: path.join(__dirname, 'config/semantic-dictionary.json'),
   MOODS_PATH: path.join(__dirname, 'config/moods.json'),
-  CONTEXTS_PATH: path.join(__dirname, 'config/contexts.json');
+  CONTEXTS_PATH: path.join(__dirname, 'config/contexts.json'),
 
 
   MAX_RETRIES: 3,
