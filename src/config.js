12854b052f3021607bc2283cffcdeb1758370514
Jimmy <antonka@vk.com>
2025-10-01 20:23:55 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -7,6 +7,10 @@ module.exports = {
   DYNAMIC_TAGS_PATH: path.join(__dirname, '../data/dynamic_tags.json'),
   TAG_STATS_PATH: path.join(__dirname, '../data/tag_statistics.json'),
   PROMPT_TEMPLATE_PATH: path.join(__dirname, 'prompt_templates/analyst_prompt.txt'),
+  SEMANTIC_DICT_PATH = path.join(__dirname, '../config/semantic-dictionary.json'),
+  MOODS_PATH = path.join(__dirname, '../config/moods.json'),
+  CONTEXTS_PATH = path.join(__dirname, '../config/contexts.json');
+
 
   MAX_RETRIES: 3,
   BASE_DELAY_MS: 2000
