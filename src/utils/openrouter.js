67ad00993e55eceb575c47e90eaeb7116e1fed96
Jimmy <antonka@vk.com>
2026-01-30 10:05:33 +0300

    Update openrouter.js

--- src/utils/openrouter.js
+++ src/utils/openrouter.js
@@ -6,11 +6,11 @@ const API_KEY = process.env.OPENROUTER_API_KEY;
 const API_URL = "https://openrouter.ai/api/v1/chat/completions"; // без пробела
 
 // Модель для генерации записей (бесплатная)
-const ESSAY_MODEL = "meta-llama/llama-3.3-70b-instruct:free";
+const ESSAY_MODEL = "mistralai/mistral-nemo:free";
 // Альтернативы: "qwen/qwen3-8b:free", "cognitivecomputations/dolphin-mistral-24b-venice-edition:free, mistralai/mistral-nemo:free"
 
 // Модель для литературного анализа (рекомендуется платная, но стабильная)
-const CRITIQUE_MODEL = "meta-llama/llama-3.3-70b-instruct:free"
+const CRITIQUE_MODEL = "mistralai/mistral-nemo:free"
 
 const today = new Date().toLocaleDateString('ru-RU', {
   day: 'numeric',
